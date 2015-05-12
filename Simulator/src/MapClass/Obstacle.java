2c20224b09414d8a09a9933b5eb682ba2793dfdb
shang418 <shang418@mit.edu>
2015-05-12 18:50:17 -0400

    working on map, obstacle and RRT classes

---
+++ Simulator/src/MapClass/Obstacle.java
@@ -0,0 +1,13 @@
+package MapClass;
+
+import java.awt.Rectangle;
+import java.awt.geom.*;
+
+public class Obstacle extends Rectangle {
+	
+		public Obstacle(int x, int y, int width, int height ){
+			this.setRect(x, y, width, height);
+		}
+		
+
+}
