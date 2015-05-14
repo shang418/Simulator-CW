d81e062f02de6078d93d3e3cb9bd508bf2b264c7
jwellons <jwellons@mit.edu>
2015-05-13 21:47:32 -0400

    updated PlayerUAV Class

--- Simulator/src/Avatars/PlayerUAV.java
+++ Simulator/src/Avatars/PlayerUAV.java
@@ -6,17 +6,19 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JLabel;
 
-public class PlayerUAV extends JLabel implements KeyListener{
+public class PlayerUAV implements KeyListener{
 	private double _x,_y,_theta;
 	static final int WIDTH = 1400;
 	static final int HEIGHT = 800;
 	int[] _startPose = {WIDTH - 50,HEIGHT - 50,0};
-	
-	Image image = GenerateImage.toImage(true);
-	JLabel player_uav = new JLabel();
+	private int _velx = 2,_vely=2;
+	private double rotvel = Math.PI/4;
 
 	public PlayerUAV(){
-			
+		//set intial position
+		_x = _startPose[0];
+		_y= _startPose[1];
+		_theta = _startPose[2];
 	}
 	
 	public double [] getPosition() {
@@ -27,31 +29,43 @@ public class PlayerUAV extends JLabel implements KeyListener{
 		    
 		    return position;
 		  }
-	
-	private void updatePose(){
-		_x = _startPose[0];
-		_y= _startPose[1];
-		_theta = _startPose[2];
 		
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		// Directional controls based off key events for player uav
+		if (e.getKeyCode() == KeyEvent.VK_A){
+			_x = _x - _velx;
+		}
+		if (e.getKeyChar() == KeyEvent.VK_D){
+			_x = _x + _velx;
+		}
+		if (e.getKeyChar() == KeyEvent.VK_W){
+			_y = _y + _vely;
+		}
+		if (e.getKeyChar() == KeyEvent.VK_S){
+			_y = _y - _vely;
+		}
+		if (e.getKeyChar() == KeyEvent.VK_I){
+			_theta = _theta + rotvel;
+		}
+		if (e.getKeyChar() == KeyEvent.VK_S){
+			_theta = _theta - rotvel;
+		}
+		// Missle Fire Event
+		if (e.getKeyChar() == KeyEvent.VK_SPACE){
+			//run missile class
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		// not used
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// not used	
 	}
 	
 
